e6b7525265cd3c5bb41c67891b5b4b5d7f44b484
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-04-16 14:58:47 -0300

    list transactions through an array

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,22 @@
   <div class="container">
     <Balance></Balance>
     <IncomeExpenses></IncomeExpenses>
-    <TransactionList></TransactionList>
+    <TransactionList :transactions="transactions"></TransactionList>
     <AddTransaction></AddTransaction>
   </div>
 </template>
 
-<script>
+<script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue'
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
 
-  export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction
-    }
-  }
+  import { ref } from 'vue';
+
+  const transactions = ref([
+        { id: 1, text: 'Flower', amount: -20},
+        { id: 2, text: 'Salary', amount: 100}
+  ]);
 </script>
