96c35e1a89d9bd043d6d88fe34988f4587901cbf
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-05-16 20:44:06 -0300

    make validation when add a transaction

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -6,7 +6,7 @@
     <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..."/>
+            <input type="text" id="text" v-model="text" placeholder="Enter text..." />
         </div>
 
         <div class="form-control">
@@ -15,7 +15,7 @@
                 <br>
                 (negative - expense, positive - income)
             </label>
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
         </div>
 
         <button class="btn">Add transaction</button>
@@ -23,7 +23,24 @@
 </template>
 
 <script setup>
-    const onSubmit = () => {
-        console.log('submited');
-    };
+
+import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
+
+const text = ref('');
+const amount = ref('');
+
+const toast = useToast();
+
+const onSubmit = () => {
+    if (!text.value || !amount.value){
+        toast.error('Both fields must be filled');
+        return;
+    }
+
+    console.log(text.value + ' - ' + amount.value);
+
+    text.value = '';
+    amount.value = '';
+};
 </script>
