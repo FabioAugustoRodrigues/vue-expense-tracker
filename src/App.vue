0cb0407a6442479a2f9c5cbac58a362ae6996cc2
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-05-16 20:56:19 -0300

    add a transaction to the list

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <Header></Header>
 
   <div class="container">
-    <Balance :total="total"></Balance>
+    <Balance :total="+total"></Balance>
     <IncomeExpenses :income="+income" :expenses="+expenses"></IncomeExpenses>
     <TransactionList :transactions="transactions"></TransactionList>
-    <AddTransaction></AddTransaction>
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
   </div>
 </template>
 
@@ -16,12 +16,16 @@ import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
+import { useToast } from 'vue-toastification';
+
 import { ref, computed } from 'vue';
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -20 },
   { id: 2, text: 'Salary', amount: 100 }
 ]);
+
+const toast = useToast();
 
 // get total
 const total = computed(() => {
@@ -49,5 +53,21 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2);
 });
+
+// add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  });
+
+  toast.success('Transaction added');
+};
+
+// generate unique id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 100000);
+}
 
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -32,13 +32,20 @@ const amount = ref('');
 
 const toast = useToast();
 
+const emit = defineEmits(['transactionSubmitted']);
+
 const onSubmit = () => {
     if (!text.value || !amount.value){
         toast.error('Both fields must be filled');
         return;
     }
 
-    console.log(text.value + ' - ' + amount.value);
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value)
+    }
+
+    emit('transactionSubmitted', transactionData)
 
     text.value = '';
     amount.value = '';
