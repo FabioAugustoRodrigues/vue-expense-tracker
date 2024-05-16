990790f364c7a52ed972816e1f0957ece7755ee2
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-05-16 20:12:36 -0300

    get the income and expense values

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,51 @@
 
   <div class="container">
     <Balance :total="total"></Balance>
-    <IncomeExpenses></IncomeExpenses>
+    <IncomeExpenses :income="income" :expenses="expenses"></IncomeExpenses>
     <TransactionList :transactions="transactions"></TransactionList>
     <AddTransaction></AddTransaction>
   </div>
 </template>
 
 <script setup>
-  import Header from './components/Header.vue';
-  import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue'
-  import TransactionList from './components/TransactionList.vue';
-  import AddTransaction from './components/AddTransaction.vue';
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 
-  import { ref, computed } from 'vue';
+import { ref, computed } from 'vue';
 
-  const transactions = ref([
-        { id: 1, text: 'Flower', amount: -20},
-        { id: 2, text: 'Salary', amount: 100}
-  ]);
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 100 }
+]);
 
-  const total = computed(() => {
-    return transactions.value.reduce((acc, transaction) => {
-      return acc+transaction.amount;
-    }, 0);
-  });
+// get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+// get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
 </script>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -6,7 +6,7 @@
             </h4>
 
             <p id="money-plus" class="money plus">
-                +$0.00
+                +${{ income }}
             </p>
         </div>
 
@@ -16,8 +16,23 @@
             </h4>
 
             <p id="money-minus" class="money minus">
-                -$0.00
+                -${{ expenses }}
             </p>
         </div>
     </div>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    income: {
+        type: Number,
+        required: true
+    },
+    expenses: {
+        type: Number,
+        required: true
+    }
+});
+</script>
